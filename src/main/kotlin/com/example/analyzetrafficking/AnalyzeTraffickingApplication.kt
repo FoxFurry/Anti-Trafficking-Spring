27c93b88c07cd5d095ff80c0c8746840999d3ed5
Isac Artur <isacartur@gmail.com>
2020-11-08 08:45:49 +0200

    Upd

--- src/main/kotlin/com/example/analyzetrafficking/AnalyzeTraffickingApplication.kt
+++ src/main/kotlin/com/example/analyzetrafficking/AnalyzeTraffickingApplication.kt
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync
 @SpringBootApplication
 class AnalyzeTraffickingApplication
 
+
 fun main(args: Array<String>) {
 	runApplication<AnalyzeTraffickingApplication>(*args)
 }
